a55c71da48a773c108f905c9a16ad5bac3292aef
sgovindaraj <salai.govindaraj@xo.com>
2014-12-18 09:50:37 -0600

    my nustone git

---
+++ config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "app": {
+        "name": "Hapi Dash",
+        "url": "http://localhost:3030"
+    },
+    "db": {
+        "host": "127.0.0.1",
+        "port": "27017",
+        "name": "hapi-dash"
+    },
+    "api": {
+        "host": "127.0.0.1",
+        "port": "3000"
+    },
+    "coreCreds": {
+        "id": "core",
+        "key": "flnvX9cpYAhxnze7aIm99esYuTZWC3sxmg5KMHvEAnex5q4AtLLM8GoYL4glO2bq",
+        "algorithm": "sha256"
+    },
+    "gui": {
+        "host": "0.0.0.0",
+        "port": "3030"
+    },
+    "email": {
+        "service": "Gmail",
+        "auth": {
+            "user": "nustoneface@gmail.com",
+            "pass": "dash1234"
+        }
+    }
+}
---
+++ config.old.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "app": {
+        "name": "NuStone",
+        "url": "http://localhost:3030"
+    },
+    "db": {
+        "host": "127.0.0.1",
+        "port": "27017",
+        "name": "hapi-dash"
+    },
+    "api": {
+        "host": "127.0.0.1",
+        "port": "3000"
+    },
+    "coreCreds": {
+        "id": "core",
+        "key": "tCBWPuAU9rUuCEkVjAnyUMGgqdWRJwImTILO59Lt9OLT2WxpdP0KqCCS0XfZ0xFc",
+        "algorithm": "sha256"
+    },
+    "gui": {
+        "host": "0.0.0.0",
+        "port": "3030"
+    },
+    "email": {
+        "service": "gsivamal@gmail.com",
+        "auth": {
+            "user": "gsivamal@gmail.com",
+            "pass": "sa"
+        }
+    }
+}
